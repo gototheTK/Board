763f95663bd1d17e832709e181cbba6c475fc65e
Jo <gototkno1@gmail.com>
2020-12-17 12:39:03 +0900

    spring

---
+++ src/main/java/org/board/controller/BoardController.java
@@ -0,0 +1,19 @@
+package org.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+
+    @GetMapping("/list")
+    public String BoardForm(Model model){
+
+
+        return "boardForm";
+    }
+
+}
