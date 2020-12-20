489537f38a4185e4cf21f2b25cce241096314959
Jo <gototkno1@gmail.com>
2020-12-20 10:10:32 +0900

    design

--- src/main/java/org/board/controller/BoardController.java
+++ src/main/java/org/board/controller/BoardController.java
@@ -1,20 +1,79 @@
 package org.board.controller;
 
+import org.board.domain.BoardVO;
+import org.board.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 @Controller
+@Log4j
 @RequestMapping("/board/*")
+@AllArgsConstructor
 public class BoardController {
+
+    private BoardService service;
 
     @GetMapping("/list")
     public String BoardForm(Model model){
 
-        
+        model.addAttribute("list", service.getList());
 
         return "boardForm";
+
     }
+
+    @PostMapping("/register")
+    public String register(BoardVO board, RedirectAttributes rttr){
+
+        log.info("register: " + board);
+
+        service.register(board);
+
+        rttr.addFlashAttribute("result", board.getBno());
+
+        return  "redirect:/board/list";
+    }
+
+    @GetMapping("/get")
+    public void get(@RequestParam("bno") Long bno, Model model){
+
+        log.info("/get");
+        model.addAttribute("board", service.get(bno));
+
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardVO board, RedirectAttributes rttr) {
+        log.info("modify:" + board);
+
+        if(service.modify(board)) {
+            rttr.addFlashAttribute("resut", "success");
+        }
+        return "redirect:/board/list";
+
+    }
+
+    @PostMapping("/remove")
+    public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr){
+
+        log.info("remove........" + bno);
+        if(service.remove(bno)){
+            rttr.addFlashAttribute("result", "success");
+        }
+        return "redirect:/board/list";
+    }
+
+    
 
 }
--- src/test/java/org/board/service/BoardServiceTests.java
+++ src/test/java/org/board/service/BoardServiceTests.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -43,6 +44,40 @@ public class BoardServiceTests {
 
     }
 
+    @Test
+    public void testGetList() {
 
+        service.getList().forEach(board -> log.info(board));
+    }
+
+    
+    @Test
+    public void testGet() {
+
+        log.info(service.get(1L));
+    }
+
+    @Test
+    public void testDelete(){
+
+        // 게시물 번호의 존재 여부를 확인하고 테스트할 것
+        log.info("REMOVE RESULT: " + service.remove(2L));
+    }
+
+    @Test
+    public void testUpdate() {
+
+        BoardVO board = service.get(1L);
+
+        if(board == null) {
+            return;
+        }
+
+        board.setTitle("제목 수정합니다.");
+        log.info("MODIFY RESULT: " + service.modify(board));
+
+    }
+
+    
 
 }
